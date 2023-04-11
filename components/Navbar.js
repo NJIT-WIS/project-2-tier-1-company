b10a7b600f6f9436322e46a5f775fcc2d8179459
SarangAP <sp49@njit.edu>
2023-04-11 17:05:37 -0400

    Menu fixes/color modifications

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import NavItem from "./NavItem"
 import React from 'react';
+import { useState } from "react";
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
@@ -10,31 +11,36 @@ const MENU_LIST = [
 //If we want to change nav menu bar appearance,
 //modify amount of divs in "nav__menu-bar"
 const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
   return (
     <header>
       <nav className={`nav`}>
         <Link href={"/"}>
-
           <h1 className="Nav">Project 2</h1>
-
         </Link>
 
-        <div className="nav__menu-bar">
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`} >
             <div></div>
             <div></div>
             <div></div>
-
         </div>
 
-        <div className="nav__menu-list">
-            {
-                MENU_LIST.map((menu, idx) => {
-                    return (
-                    <div key={menu.text}>
-                       <NavItem {...menu} />
-                    </div>
-                );
-            })}
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+              >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
         </div>
       </nav>
     </header>

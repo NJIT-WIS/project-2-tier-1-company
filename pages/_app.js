a6c8c102fd1f508ae9952aaf752bceec9c96f55d
SarangAP <sp49@njit.edu>
2023-04-11 14:51:57 -0400

    Added NavItem.js and modified _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,16 @@ import '../styles/global.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+//Uncomment after nav bar js file created
+/*
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
+*/

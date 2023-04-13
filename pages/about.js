741dd9e9f56879b294ebe8c91a90012dca9f75e0
Sarang P <106339241+SarangAP@users.noreply.github.com>
2023-04-13 11:48:48 -0400

    Removing about section
    Removing due to errors being caused, testing if errors are persistent

--- pages/about.js
+++ pages/about.js
@@ -15,18 +15,6 @@ export default function Home({ allPostsData }) {
         <p>This is the about page</p>
         <h1><center>Our Team</center></h1>
         <h1><center>Our Mission</center></h1>
-          <p><center>Our mission is to empower individuals with the knowledge and skills
-              they need to achieve their dreams and succeed in today's rapidly changing world.
-              We believe that everyone deserves access to quality education, regardless of their background or
-              circumstances. That's why we've created an innovative online platform, designed to provide engaging and
-              interactive learning experiences that are accessible to anyone, anywhere. At our core, we are educators
-              and innovators, committed to transforming the way people learn and grow. We are passionate about
-              empowering our students to reach their full potential and make a positive impact in their communities
-              and beyond. With our cutting-edge technology and expert instructors, we offer a dynamic and personalized
-              learning experience that helps our students thrive. Whether you're looking to advance your career, pursue
-              your passions, or simply learn something new, our platform provides the tools and resources you need to
-              succeed. Join us on our mission to empower learners everywhere and unlock the limitless potential of
-              education.</center></p>
         <h1><center>Our Partnerships</center></h1>
         <h1><center>Agile Overview</center></h1>
       </section>

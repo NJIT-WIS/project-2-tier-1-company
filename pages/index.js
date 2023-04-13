4eb19850f212bdce9b1dbeda38c8a071caf99add
Eric Huang <eyh3@njit.edu>
2023-04-12 20:30:08 -0400

    New homepage layout

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import Image from 'next/image'
-import banner from './/img/testbanner2.jpg'
+import banner from './/img/computer.jpg'
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,28 +13,14 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Image src={banner} width={1500} height={300} />
+      <Image src={banner} width={400} height={300} id="banner"/>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p><h1>Introducing mywebclass.org!</h1> </p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          A new learning resourec for educators in our AI driven world.
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`} legacyBehavior>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+
     </Layout>
   );
 }

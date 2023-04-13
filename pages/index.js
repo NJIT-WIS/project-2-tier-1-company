57ba52f6adfacd89a510234b2ae77d75d4ed31c8
Eric Huang <eyh3@njit.edu>
2023-04-13 12:04:38 -0400

    fix hydration error

--- pages/index.js
+++ pages/index.js
@@ -17,17 +17,17 @@ export default function Home({ allPostsData }) {
       <Image src={hero} width={950} height={700}/>
       <Image src={banner} width={400} height={300} className="rightimg"/>
       <section className={utilStyles.headingMd}>
-        <p><h1>Introducing mywebclass.org!</h1></p>
+        <h1>Introducing mywebclass.org!</h1>
         <p>
           A new learning resource for educators in our AI driven world.
         </p>
       </section>
     <section className={utilStyles.headingMd}>
-      <p><h3>Section 2</h3></p>
+      <h3>Section 2</h3>
       <p>Paragraph 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
     <section className={utilStyles.headingMd}>
-      <p><h3>Section 3</h3></p>
+      <h3>Section 3</h3>
       <p>Paragraph 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
     </Layout>

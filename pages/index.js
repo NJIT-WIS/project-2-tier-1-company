630b85e3769eeb734f82ab7b83ea2cb8385d5411
Eric Huang <eyh3@njit.edu>
2023-04-12 21:48:30 -0400

    filler text

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,21 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Image src={banner} width={400} height={300} id="banner"/>
+      <Image src={banner} width={400} height={300} className="rightimg"/>
       <section className={utilStyles.headingMd}>
         <p><h1>Introducing mywebclass.org!</h1></p>
         <p>
           A new learning resource for educators in our AI driven world.
         </p>
       </section>
-
+    <section className={utilStyles.headingMd}>
+      <p><h3>Section 2</h3></p>
+      <p>Paragraph 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
+    <section className={utilStyles.headingMd}>
+      <p><h3>Section 3</h3></p>
+      <p>Paragraph 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
     </Layout>
   );
 }

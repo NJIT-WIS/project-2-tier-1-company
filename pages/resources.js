badf44893f6418ba8c0045af3f0c7c6679dfda10
Eric Huang <eyh3@njit.edu>
2023-04-15 16:16:38 -0400

    formatting for resources page

--- pages/resources.js
+++ pages/resources.js
@@ -4,6 +4,8 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { BsFiletypeDoc } from 'react-icons/bs'
+import { BsFiletypePdf } from 'react-icons/bs'
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,10 +15,13 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1><center>Academic Articles</center></h1>
-        <Link href={`resources`} legacyBehavior><a><h3>Article 1 - aiva avnabn 09a n0 an90a ean0an aej sgoj3</h3></a></Link>
-        <Link href={`resources`} legacyBehavior><a><h3>Article 2 - jv n oni o oo uyb6 t7 0 09oii hg</h3></a></Link>
-        <Link href={`resources`} legacyBehavior><a><h3>Article 3 - ign o eoiew ti0 w-0 ad asefegj ieojf popoi iie9i0w i00ww 2jfjgoa jjjiii</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3><BsFiletypeDoc/> Article 1 - aiva avnabn 09a n0 an90a ean0an aej sgoj3</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3><BsFiletypeDoc/> Article 2 - jv n oni o oo uyb6 t7 0 09oii hg</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3><BsFiletypeDoc/> Article 3 - ign o eoiew ti0 w-0 adgj ieojf popoi iie9i0w i00ww 2jfjgoa jjjiii</h3></a></Link>
         <h1><center>Downloadable Guides</center></h1>
+        <Link href={`resources`} legacyBehavior><a><h3><BsFiletypePdf/> Guide 1 - aiva avnabn 09a n0 an90a ean0an aej sgoj3</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3><BsFiletypePdf/> Guide 2 - jv n oni o oo uyb6 t7 0 09oii hg</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3><BsFiletypePdf/> Guide 3 - ign o eoiew ti0 w-0 adgj ieojf popoi iie9i0w i00ww 2jfjgoa jjjiii</h3></a></Link>
       </section>
 
 

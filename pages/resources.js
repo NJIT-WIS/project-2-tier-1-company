35661f367115fecd48d83d94b38483b1d151d603
Eric Huang <eyh3@njit.edu>
2023-04-15 14:49:57 -0400

    resouces page

--- pages/resources.js
+++ pages/resources.js
@@ -12,8 +12,10 @@ export default function Home({ allPostsData }) {
         <title>MyWebClass.org | Resources</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p><center>This is the Resources page</center></p>
         <h1><center>Academic Articles</center></h1>
+        <Link href={`resources`} legacyBehavior><a><h3>Article 1 - aiva avnabn 09a n0 an90a ean0an aej sgoj3</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3>Article 2 - jv n oni o oo uyb6 t7 0 09oii hg</h3></a></Link>
+        <Link href={`resources`} legacyBehavior><a><h3>Article 3 - ign o eoiew ti0 w-0 ad asefegj ieojf popoi iie9i0w i00ww 2jfjgoa jjjiii</h3></a></Link>
         <h1><center>Downloadable Guides</center></h1>
       </section>
 

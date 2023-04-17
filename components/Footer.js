c07b7e468dbd9124bd097e76d575d03804db8d59
SarangAP <sp49@njit.edu>
2023-04-17 18:51:22 -0400

    Added Footer component

---
+++ components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.socialIcons}>
+        {}
+        <a
+          href="https://www.facebook.com/"
+          className={styles.socialIconLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/facebook-icon.png" alt="Facebook" />
+        </a>
+        <a
+          href="https://www.twitter.com/"
+          className={styles.socialIconLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/twitter-icon.png" alt="Twitter" />
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          className={styles.socialIconLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/instagram-icon.png" alt="Instagram" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

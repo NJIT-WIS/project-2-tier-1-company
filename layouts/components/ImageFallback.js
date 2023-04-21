ac50b34c09054964809627f541a482949d17b8c5
SarangAP <sp49@njit.edu>
2023-04-20 21:12:20 -0400

    Added blog img check

--- layouts/components/ImageFallback.js
+++ layouts/components/ImageFallback.js
@@ -9,8 +9,8 @@ const ImageFallback = (props) => {
     const currentLocation = window.location.href;
     const isDeployedOnGitHubPages = currentLocation.includes("github.io");
 
-    //Checking to see if image source for blogs
-    const resolvedSrc =
+`    //Checking to see if image source for blogs
+`    const resolvedSrc =
       src.startsWith("/images/blog") || src.startsWith("/images/author")
         ? (isDeployedOnGitHubPages ? "/project-2-tier-1-company" : "") + src
         : src;

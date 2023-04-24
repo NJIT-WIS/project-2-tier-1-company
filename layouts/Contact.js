711677232c43890d51ece7b8b489b21064ad55ef
Eric Huang <eyh3@njit.edu>
2023-04-23 23:33:31 -0400

    separate divs

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -37,11 +37,11 @@ const Contact = ({ data }) => {
                   type="email"
                   required
                 />
-                <div className="my-5">
+          </div>
+          <div className="my-5">
                 <input type="checkbox" id="agree" name="agree" value="agree"/>
                 <label for="agree"> I agree to the Terms of Service and Privacy Policy</label>
-                </div>
-          </div>
+            </div>
           <div className="animate lg:col-5 my-5">
             <form
               method="POST"

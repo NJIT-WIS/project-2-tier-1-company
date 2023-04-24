4834ee55aad865ea20a161db9b97be86fa3495c0
Eric Huang <eyh3@njit.edu>
2023-04-23 23:58:29 -0400

    trying to center a button

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -44,8 +44,8 @@ const Contact = ({ data }) => {
                 <input type="checkbox" id="agree" name="agree" value="agree"/>
                 <label for="agree">  I agree to the Terms of Service and Privacy Policy</label>
             </div>
-          <div className="lg:col-4 my-5 text-center items-center">
-            <button className="btn btn-primary block w-full lg:col-4">
+          <div className="my-5 text-center">
+            <button className="btn btn-primary block w-50">
                 Subscribe!
             </button>
           </div>

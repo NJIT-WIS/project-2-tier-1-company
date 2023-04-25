a3fd2d5c55251378def67c467ecc721f23e5c5de
km776 <km776@njit.edu>
2023-04-24 20:49:26 -0400

    changed favicon and logo font

--- pages/index.js
+++ pages/index.js
@@ -439,7 +439,7 @@ const Home = ({ banner, brands, features, intro, speciality, testimonial }) => {
                               "p",
                               "text-[17px] lg:text-lg text-body mt-4 md:mt-5 xl:mt-8"
                             )}
-                            <div className="mt-7 inline-block rounded-md bg-body p-7 shadow-[0_10px_50px_rgba(0,0,0,.08)] md:mt-5 lg:mt-8 xl:mt-5"> //*
+                            <div className="mt-7 inline-block rounded-md bg-body p-7 shadow-[0_10px_50px_rgba(0,0,0,.08)] md:mt-5 lg:mt-8 xl:mt-5">
                               <ImageFallback
                                 className="mx-auto rounded-full"
                                 src={item.avatar}

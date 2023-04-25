8908a745d04d187ffdb00db17a79fec4342cf440
SarangAP <sp49@njit.edu>
2023-04-24 22:20:52 -0400

    Modified about page

--- layouts/About.js
+++ layouts/About.js
@@ -258,36 +258,6 @@ const About = ({ data }) => {
           </div>
         </div>
 
-        {/* Members */}
-        <div className="section container">
-          <div className="animate text-center">
-            <p>{our_member.subtitle}</p>
-            {markdownify(our_member.title, "h2", "section-title mt-4")}
-            {markdownify(our_member.content, "p", "mt-16")}
-          </div>
-          <div className="row justify-center">
-            <div className="lg:col-10">
-              <div className="row">
-                {our_member.list.map((member, index) => (
-                  <div
-                    key={("member-", index)}
-                    className="animate mt-10 text-center md:col-6 lg:col-4"
-                  >
-                    <ImageFallback
-                      className="mx-auto rounded-full shadow-[10px_10px_0] shadow-primary/10"
-                      src={member.image}
-                      width={245}
-                      height={245}
-                      alt={member.name}
-                    />
-                    <h4 className="mt-8">{member.name}</h4>
-                    <p className="mt-3">{member.field}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
 
         {/* Office */}
         <div className="section container">

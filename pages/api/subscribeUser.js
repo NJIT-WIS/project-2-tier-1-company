786f47fe203d09a8ce7cc2e6d7101e800f4b5102
Eric Huang <eyh3@njit.edu>
2023-04-27 11:05:15 -0400

    changed error handling

--- pages/api/subscribeUser.js
+++ pages/api/subscribeUser.js
@@ -38,6 +38,6 @@ export default async (req, res) => {
 
     return res.status(201).json({ error: '' });
   } catch (error) {
-    return res.status(504).json({ error: error.message || error.toString() });
+    return res.status(405).json({ error: error.message || error.toString() });
   }
 };
